c84fef5ef0125cd7fc51ccf4d01e9e6e9c21f996
Yang <andy.yang05@sap.com>
2018-01-12 21:26:37 +0800

    update

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -35,12 +35,13 @@
                 var wxConfig = await getJsSdk({
                     url: window.location.href.split('#')[0]
                 })
+                console.log(1)
                 console.log(wxConfig)
                 wx.config({
                     debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-                    appId: wxConfig.appid, // 必填，公众号的唯一标识
+                    appId: wxConfig.appId, // 必填，公众号的唯一标识
                     timestamp: wxConfig.timestamp, // 必填，生成签名的时间戳
-                    nonceStr: wxConfig.noncestr, // 必填，生成签名的随机串
+                    nonceStr: wxConfig.nonceStr, // 必填，生成签名的随机串
                     signature: wxConfig.signature, // 必填，签名，见附录1
                     jsApiList: ['onMenuShareTimeline', 'onMenuShareAppMessage'] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
                 })
